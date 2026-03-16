e8801aaf028336b0618dac06370bf2d262f02f6f
Commax <commaxweb@proton.me>
2026-03-16 16:43:47 -0300

    Commit #3: Implementação da persistência e listagem de produtos com sucesso

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -23,20 +23,46 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+    //
+        conn = new conectaDAO().connectDB(); 
         
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
         
-        //conn = new conectaDAO().connectDB();
-        
-        
-    }
-    
+        try {
+            // Agora o 'conn' não é mais nulo, e o erro para!
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + e.getMessage());
+     }
+    }    
     public ArrayList<ProdutosDTO> listarProdutos(){
+conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos";
         
-        return listagem;
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                listagem.add(produto);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar: " + e.getMessage());
+        }        return listagem;
     }
     
     
     
         
 }
-
--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -22,9 +22,8 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
+            //conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?user=root&password=root&useSSL=false");        } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
